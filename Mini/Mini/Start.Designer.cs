199137ea6b30f770686b56913bfa2ffefb368b32
bobcristian <bobcristian96@yahoo.com>
2017-05-24 23:43:17 +0300

    Initial commit
    Need windows bindings

---
+++ Mini/Mini/Start.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Mini
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Start));
+            this.colTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // colTextBox
+            // 
+            this.colTextBox.Location = new System.Drawing.Point(288, 162);
+            this.colTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.colTextBox.Name = "colTextBox";
+            this.colTextBox.Size = new System.Drawing.Size(70, 40);
+            this.colTextBox.TabIndex = 0;
+            this.colTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.colTextBox_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 165);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Column Number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(149, 40);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 39);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Start";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(135, 294);
+            this.NextButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(150, 44);
+            this.NextButton.TabIndex = 5;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "S";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(321, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "D";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(312, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "E";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(87, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "D";
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(403, 378);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.colTextBox);
+            this.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Start";
+            this.Text = "Start";
+            this.BindingContextChanged += new System.EventHandler(this.Start_BindingContextChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox colTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Mini/Mini/Tabel.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mini
+{
+    public partial class Tabel : Form
+    {
+        public void UpdateDataGrid(int col)
+        {
+            //Columns
+            char ev = 'a';
+            StatesView.ColumnCount = col;
+            for(int i=0;i<col;i++)
+            {
+                StatesView.Columns[i].Name = ev.ToString();
+                ev++;
+            }
+        }
+
+        public Tabel(int col)
+        {
+            InitializeComponent();
+            UpdateDataGrid(col);
+            GenTableButton.Enabled = false;
+            NewTableButton.Enabled = false;
+            EchivClassesButton.Enabled = false;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            int rows = StatesView.RowCount-1;
+            int columns = StatesView.ColumnCount;
+
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter("mat.txt", false))
+                {
+                    file.Write(rows);
+                    file.Write(" ");
+                    file.WriteLine(columns);
+
+                    for (int i = 0; i < StatesView.RowCount - 1; i++)
+                    {
+                        for (int j = 0; j < StatesView.ColumnCount; j++)
+                        {
+                            if (StatesView.Rows[i].Cells[j].Value != null)
+                            {
+                                file.Write(System.Convert.ToInt16(StatesView.Rows[i].Cells[j].Value.ToString()));
+                                file.Write(" ");
+                            }
+                            else
+                            {
+                                throw new Exception("Do not leave a blank cell");
+                            }
+                        }
+                        file.WriteLine("");
+                    }
+                    file.Close();
+                }
+
+                System.Diagnostics.Process.Start("Minimiz.exe");
+                GenTableButton.Enabled = true;
+                NewTableButton.Enabled = true;
+                EchivClassesButton.Enabled = true;
+                MessageBox.Show("Successful minimization!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void StatesView_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            var grid = sender as DataGridView;
+            var rowIdx = (e.RowIndex + 1).ToString();
+
+            var centerFormat = new StringFormat()
+            {
+                // right alignment might actually make more sense for numbers
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            var headerBounds = new Rectangle(e.RowBounds.Left, e.RowBounds.Top, grid.RowHeadersWidth, e.RowBounds.Height);
+            e.Graphics.DrawString(rowIdx, this.Font, SystemBrushes.ControlText, headerBounds, centerFormat);
+        }
+
+        private void Tabel_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void GenTableButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (System.IO.StreamReader sr = new System.IO.StreamReader("Tabel_Minimizat.txt"))
+                {
+                    String line = sr.ReadToEnd();
+                    TableMin.Text = line;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Minimiz.exe");
+        }
+
+        private void EchivClassesButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (System.IO.StreamReader sr = new System.IO.StreamReader("Clase_Echivalente.txt"))
+                {
+                    String line = sr.ReadToEnd();
+                    TableMin.Text = line;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void NewTableButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (System.IO.StreamReader sr = new System.IO.StreamReader("Tabel_Nou.txt"))
+                {
+                    String line = sr.ReadToEnd();
+                    TableMin.Text = line;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
